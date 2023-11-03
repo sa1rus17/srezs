b9c835d3b66155765fea2990b5375e9119fe9fd9
Андрей Муллакаев <mullakaev-ae@mail.ru>
2023-11-03 23:09:24 +0700

    1

---
+++ srez.py
@@ -0,0 +1,19 @@
+a=input('Введите строку:')
+#1
+print(a[2])
+#2
+print(a[-2])
+#3
+print(a[:5])
+#4
+print(a[:-2])
+#5
+print(a[::2])
+#6
+print(a[1::2])
+#7
+print(a[::-1])
+#8
+print(a[-1:1])
+#9
+print(len(a))
